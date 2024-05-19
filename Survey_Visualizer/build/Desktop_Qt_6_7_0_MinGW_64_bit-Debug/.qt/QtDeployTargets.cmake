403b632df4091a7621bd23347d1b59f8ddbd22e7
Dillon_Pullano <132423399+dgpullano@users.noreply.github.com>
2024-05-18 21:28:21 -0600

    Created initial Qt project using Model-View-Controller (MVC) architecture.

---
+++ Survey_Visualizer/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Survey_Visualizer_FILE C:/Users/Dillon/Desktop/Survey-Visualizer/Survey_Visualizer/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/Survey_Visualizer.exe)
+set(__QT_DEPLOY_TARGET_Survey_Visualizer_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
